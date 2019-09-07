8a6fe0e40dfaee7074c4c922144e713303b332de
theux1 <54308103+theux1@users.noreply.github.com>
2019-09-07 18:26:11 +0200

    create a js.file

---
+++ javascript/scripts.js
@@ -0,0 +1,6 @@
+function pictureChange()
+ {
+  document.getElementById("theCurtain").src="img/Communication/Mobile/curtain-open-854x4399.svg";
+ }
+
+console.log("Are you ready for some perceptional secrets?")
