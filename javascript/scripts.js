c4270e39b83c965b5c9acd90ccc7524a6c6a047d
theux1 <54308103+theux1@users.noreply.github.com>
2019-09-09 15:56:04 +0200

    beautify code

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,7 +1,8 @@
-function pictureChange()
- {
-  document.getElementById("thecurtain").src="img/Communication/Mobile/curtain-open-854x4399.svg";
- }
+
+
+function pictureChange() {
+  document.getElementById("thecurtain").src = "img/Communication/Mobile/curtain-open-854x4399.svg";
+}
 
 console.log("Are you ready for some perceptional secrets?")
 
@@ -9,15 +10,28 @@ function startAnimation() {
   /* the idea is to set a class .paused to freeze the animation when the site renders right away,
   then remove the class via JS, the animation will then continue*/
   var element = document.getElementById("anitiger");
-element.classList.remove("paused");
+  element.classList.remove("paused");
   var element = document.getElementById("anibush");
-element.classList.remove("paused");
+  element.classList.remove("paused");
   var element = document.getElementById("anicoming");
-element.classList.remove("paused");
+  element.classList.remove("paused");
   var element = document.getElementById("anibestgone");
-element.classList.remove("paused");
-var element = document.getElementById("anigoing");
-element.classList.remove("paused");
+  element.classList.remove("paused");
+  var element = document.getElementById("anigoing");
+  element.classList.remove("paused");
   var element = document.getElementById("anihehe");
-element.classList.remove("paused");
+  element.classList.remove("paused");
 }
+
+/* Will just reset the paused state; not the css animation
+
+function resetAnimation() {
+
+  var element = document.getElementById("anitiger").reset();
+  var element = document.getElementById("anibush").reset();
+  var element = document.getElementById("anicoming").reset();
+  var element = document.getElementById("anibestgone").reset();
+var element = document.getElementById("anigoing").reset();
+  var element = document.getElementById("anihehe").reset();
+}
+*/
