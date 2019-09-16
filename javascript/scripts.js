30daae49caefb145085bb421c97cd0f3c0895e4e
theux1 <54308103+theux1@users.noreply.github.com>
2019-09-16 15:33:03 +0200

    unify js code

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,3 @@
-
 // open curtain by changing imgs
 function pictureChange() {
   document.getElementById("thecurtain").src = "img/Communication/Mobile/curtain-open-854x4399.svg";
@@ -10,101 +9,93 @@ function closeCurtain() {
 }
 // adding a hamburger menu
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+  var x = document.getElementById("myNavtoggle");
 
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive';
-  }
-
-  else {
-    x.className = 'navtoggle';
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
   }
 }
 
 // Please ignore it! Just some messing around
-console.log("Are you ready for some perceptional secrets?")
-
+console.log("Are you ready for some perceptional secrets?");
 
 // Starting the animation
 function startAnimation() {
   /* the idea is to set a class .paused to freeze the animation when the site renders right away,
-  then remove the class via JS, the animation will then continue*/
-  var element = document.getElementById("anitiger");
-  element.classList.remove("paused");
-  var element = document.getElementById("anibush");
-  element.classList.remove("paused");
-  var element = document.getElementById("anicoming");
-  element.classList.remove("paused");
-  var element = document.getElementById("anibestgone");
-  element.classList.remove("paused");
-  var element = document.getElementById("anigoing");
-  element.classList.remove("paused");
-  var element = document.getElementById("anihehe");
-  element.classList.remove("paused");
+  then remove the class via JS, the animation will then continue */
+  var element1 = document.getElementById("anitiger");
+  element1.classList.remove("paused");
+  var element2 = document.getElementById("anibush");
+  element2.classList.remove("paused");
+  var element3 = document.getElementById("anicoming");
+  element3.classList.remove("paused");
+  var element4 = document.getElementById("anibestgone");
+  element4.classList.remove("paused");
+  var element5 = document.getElementById("anigoing");
+  element5.classList.remove("paused");
+  var element6 = document.getElementById("anihehe");
+  element6.classList.remove("paused");
 }
 
 // reset svg var
-var reloadButton  = document.querySelector( '.reload' );
-var reloadSvg     = document.querySelector( '#reloadbutton' );
+var reloadButton = document.querySelector(".reload");
+var reloadSvg = document.querySelector("#reloadbutton");
 var reloadEnabled = true;
-var rotation      = 0;
+var rotation = 0;
 
 // reset svg Events
-reloadButton.addEventListener('click', function() { reloadClick() });
+reloadButton.addEventListener("click", function() {
+  reloadClick();
+});
 
 // reset svg Functions
 function reloadClick() {
-
   reloadEnabled = false;
   rotation -= 180;
-
-reloadSvg.style.webkitTransform = 'translateZ(0px) rotateZ( ' + rotation + 'deg )';
-reloadSvg.style.MozTransform  = 'translateZ(0px) rotateZ( ' + rotation + 'deg )';
-reloadSvg.style.transform  = 'translateZ(0px) rotateZ( ' + rotation + 'deg )';
+  reloadSvg.style.webkitTransform = "translateZ(0px) rotateZ( " + rotation + "deg )";
+  reloadSvg.style.MozTransform = "translateZ(0px) rotateZ( " + rotation + "deg )";
+  reloadSvg.style.transform = "translateZ(0px) rotateZ( " + rotation + "deg )";
 }
 // Show reset svg
 setTimeout(function() {
-  reloadButton.classList.add('active');
+  reloadButton.classList.add("active");
 }, 1);
-
-
 
 // click eventt jquery
 
 $("#reloadbutton").click(function() {
-
-//add the formerly removed paused class
+  // add the formerly removed paused class
   $("#animation").find("img").addClass("paused");
+  // get img by id, then clone the img, insert, and remove the old img by id
+  var el1 = $("#anibush"),
+    newone1 = el1.clone(true);
+  el1.before(newone1);
+  $("." + el1.attr("id") + ":last").remove();
 
-//get img by id, then clone the img, insert, and remove the old img by id
-   var el     = $("#anibush"),
-       newone = el.clone(true);
-   el.before(newone);
-   $("." + el.attr("id") + ":last").remove();
+  var el2 = $("#anitiger"),
+    newone2 = el2.clone(true);
+  el2.before(newone2);
+  $("." + el2.attr("id") + ":last").remove();
 
-   var el     = $("#anitiger"),
-       newone = el.clone(true);
-   el.before(newone);
-   $("." + el.attr("id") + ":last").remove();
+  var el3 = $("#anicoming"),
+    newone3 = el3.clone(true);
+  el3.before(newone3);
+  $("." + el3.attr("id") + ":last").remove();
 
-   var el     = $("#anicoming"),
-       newone = el.clone(true);
-   el.before(newone);
-   $("." + el.attr("id") + ":last").remove();
+  var el4 = $("#anibestgone"),
+    newone4 = el4.clone(true);
+  el4.before(newone4);
+  $("." + el4.attr("id") + ":last").remove();
 
-   var el     = $("#anibestgone"),
-       newone = el.clone(true);
-   el.before(newone);
-   $("." + el.attr("id") + ":last").remove();
+  var el5 = $("#anigoing"),
+    newone5 = el5.clone(true);
+  el5.before(newone5);
+  $("." + el5.attr("id") + ":last").remove();
 
-   var el     = $("#anigoing"),
-       newone = el.clone(true);
-   el.before(newone);
-   $("." + el.attr("id") + ":last").remove();
-
-   var el     = $("#anihehe"),
-       newone = el.clone(true);
-   el.before(newone);
-   $("." + el.attr("id") + ":last").remove();
-
-  });
+  var el6 = $("#anihehe"),
+    newone6 = el6.clone(true);
+  el6.before(newone6);
+  $("." + el6.attr("id") + ":last").remove();
+});
